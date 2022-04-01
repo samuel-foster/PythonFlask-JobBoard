ca18a95f8942b40ed1847b0ed6d832e52c54e02e
samuel-foster <47427579+samuel-foster@users.noreply.github.com>
2022-04-01 12:28:09 +0100

    minor changes

--- jobs/app.py
+++ jobs/app.py
@@ -49,8 +49,8 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-@app.route('/employer/<employer_id>/review')
-def review(employer_id, methods = ('GET', 'POST')):
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
     if request.method == 'POST':
         review = request.form['review']
         rating = request.form['rating']
@@ -59,7 +59,7 @@ def review(employer_id, methods = ('GET', 'POST')):
         date =datetime.datetime.now().strftime("%m/%d/%Y")
 
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
-        return redirect(url_for('employer.html',employer_id=employer_id))
+        return redirect(url_for('employer',employer_id=employer_id))
     
     return render_template('review.html', employer_id=employer_id)
 
